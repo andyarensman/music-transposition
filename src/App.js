d4033cb52ff6f1ae0b1a7790cac33de68a2ba1e0
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-12-14 17:10:44 -0800

    playing with someone's example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Score } from './Score'
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div id="output"></div>
-    </div>
-  );
+    <Score
+      staves={[
+        ['g3', 'd4', 'e4', 'd4'],
+        ['a4', 'd4', 'e4', 'd4'],
+        ['a4', 'a4', 'b4', 'a4'],
+        ['d4', 'e4', ['g3', 2]],
+      ]}
+    />
+  )
 }
 
 export default App;
